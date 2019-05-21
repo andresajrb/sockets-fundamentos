ef1f39e5ec04081ab8039741946a9a4ca695e096
Andres <andresajrb@gmail.com>
2019-05-21 09:22:52 -0400

    Primer commit de la aplicacion de fundamentos de Sockets

---
+++ server/server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+
+const path = require('path');
+
+const app = express();
+let server = http.createServer(app);
+
+const publicPath = path.resolve(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+// IO = esta es la comunicacion del backend
+let io = socketIO(server);
+
+io.on('connection', (client) => {
+    console.log('Usuario conectado');
+
+    client.emit('enviarMensaje', {
+        usuario: 'Administrador',
+        mensaje: 'Bienvenido a esta aplicacion'
+    });
+
+    client.on('disconnect', () => {
+        console.log('Usuario desconectado');
+    });
+
+    // Escuchar el cliente
+    client.on('enviarMensaje', (data, callback) => {
+        console.log(data);
+
+        client.broadcast.emit('enviarMensaje', data);
+
+        // if (mensaje.usuario) {
+        //     callback({
+        //         resp: 'TODO SALIO BIEN!'
+        //     })
+        // } else {
+        //     callback({
+        //         resp: 'TODO SALIO MAL!!!!!!!!!!!'
+        //     })
+        // }
+
+        // callback();
+    })
+});
+
+
+server.listen(port, (err) => {
+
+    if (err) throw new Error(err);
+
+    console.log(`Servidor corriendo en puerto ${ port }`);
+
+});
